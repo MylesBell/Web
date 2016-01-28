2f7f53e4a0467460609463a4eb6fcfabded66f6f
James Hughes <jh13293@my.bristol.ac.uk>
2016-01-28 20:41:38 +0000

    actually add it

---
+++ rushmore/app/components/services/SpecialPowerManagerService.js
@@ -0,0 +1,28 @@
+/*
+    Service that provides an interface for sending and recieving messages over the network,
+    connects to mobile server using socketio client
+*/
+angular.module('myApp').factory('SpecialPowerManagerService', function($q, $interval) {
+
+    var cooldownTIme = 5000;
+
+    var specialButtonUsed = function() {
+        var deferred = q.defer();
+
+        var specialCooldownTimer = $interval(function() {
+            $interval.cancel(specialCooldownTimer);
+            deferred.resolve();
+        }, cooldownTIme);
+
+        return deferred.promise;
+    }
+
+    /* --------------------
+         PUBLIC API
+     ---------------- */
+    return {
+        send: send,
+        registerListener: registerListener
+    };
+
+});
