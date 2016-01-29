2a0f48246648e43f0bc438e016d56444bec7bf11
James Hughes <jh13293@my.bristol.ac.uk>
2016-01-29 17:12:49 +0000

    fix tests

--- socket_server/server.js
+++ socket_server/server.js
@@ -102,7 +102,7 @@ io.on('connection', function(socket) {
             var unityRes = unity.gamePlayerJoined(socket, {
                 playerID: socket.id,
                 teamID: 1,
-                state: 1, // 0 the state for idle (needed for testing)                
+                state: 0, // 0 the state for idle (needed for testing)                
                 ok: 1 // code was correct
             }, housekeeping.logger, playerList);
 
