51a89a658c1bbae237eab8667b4f2da900b1c515
josephfoulds <josephfoulds@googlemail.com>
2016-02-02 13:35:37 +0000

    Fix up commenting across the server.js

--- socket_server/server.js
+++ socket_server/server.js
@@ -4,13 +4,14 @@
  *
 */
 
-// Require the SocketIO library
+// Require the dependencies for this interface
 var socketio = require('socket.io');
 var logger = require('./logger');
 var mobile = require('./mobile');
 var unity = require('./unity');
 var utils = require('./utils');
 
+// Default to port 1337
 var port = 1337;
 
 // Start listening on a non-root-locked port
@@ -19,10 +20,10 @@ var io = socketio.listen(port);
 var UNITY_CHAN = "unity";
 var MOBILE_CHAN = "mobile";
 
-// Array to store list of players in a game
+// Global array to store list of players in a game
 var playerList = [];
 
-// Get the logging level from the command line
+// Get the logging level from the command line (defaults to FULL)
 var loggingLevel = process.argv.slice(2)[0];
 if (loggingLevel !== undefined) {
     loggingLevel = loggingLevel.split("=")[1];
@@ -30,7 +31,7 @@ if (loggingLevel !== undefined) {
     loggingLevel = "FULL";
 }
 
-// Get testing level from command line
+// Get testing level from command line (defaults to FALSE)
 var testingEnabled = process.argv.slice(3)[0];
 if (testingEnabled !== undefined) {
     testingEnabled = testingEnabled.split("=")[1];
@@ -41,37 +42,39 @@ if (testingEnabled !== undefined) {
     testingEnabled = false;
 }
 
-console.log("socket server listening on " + port + " logging set to " + loggingLevel + " testing is " + testingEnabled);
+// Notify that the server has begun listening and the command line parameters provided
+console.log("Socket server listening on port: " + port);
+console.log("Logging set to: " + loggingLevel);
+console.log("Testing set to: " + testingEnabled);
 
+// These functions are exposed to a SocketIO connection
 io.on('connection', function(socket) {
 
+    // Define a new logging instance for this connection
     var housekeeping = new logger();
+
+    // Set the logging level to the provided logging parameter from command line
     housekeeping.setLoggingLevel(loggingLevel);
 
+    // Fire that a new connection has been made (this just logs it as we implement our own namespacing)
     utils.connect(socket, housekeeping.logger);
 
+
+    /* 
+     ----------------------
+      Housekeeping events 
+     ----------------------
+    */
+
+    /*
+        Client has subscribed to a channel - our namespace implementation
+    */
     socket.on('subscribe', function(data) {
         utils.subscribe(socket, data, housekeeping.logger);
     });
 
-    /* 
-     ----------------------
-      Mobile events 
-     ----------------------
-    */
-
     /*
-        New player wants to register in the system with a name and socket id
-    */
-    socket.on('playerRegister', function(data, callback) {
-        var res = mobile.playerRegister(socket, data, housekeeping.logger);
-
-        // Return the response back to the client, either success or failure, to fufilled the promise
-        callback(res);
-    });
-
-    /*
-        Player closes the browser or leaves the game
+        Client closes the browser or leaves the game
 
         Remove them from the player list, update everyone else's player list 
         and tell unity that player has left
@@ -87,7 +90,21 @@ io.on('connection', function(socket) {
                 io.sockets.in(pl.uID).emit('gamePlayerLeft', res);
             });
         }
+    });
 
+    /* 
+     ----------------------
+      Mobile events 
+     ----------------------
+    */
+    /*
+        New player wants to register in the system with a name and socket id
+    */
+    socket.on('playerRegister', function(data, callback) {
+        var res = mobile.playerRegister(socket, data, housekeeping.logger);
+
+        // Return the response back to the client, either success or failure, to fufilled the promise
+        callback(res);
     });
 
     /*
@@ -126,7 +143,9 @@ io.on('connection', function(socket) {
         callback(res);
     });
 
-
+    /*
+        Player has fired a special attack button
+    */
     socket.on('playerSpecial', function(data) {
         var res = mobile.playerSpecial(socket, data, housekeeping.logger);
 
@@ -135,6 +154,9 @@ io.on('connection', function(socket) {
         }
     });
 
+    /*
+        Player has attempted to switch direction of movement   
+    */
     socket.on('playerDirection', function(data) {
         var res = mobile.playerDirection(socket, data, housekeeping.logger);
         console.log(res);
@@ -143,6 +165,9 @@ io.on('connection', function(socket) {
         }
     });
 
+    /*
+        Player has attempted to switch which base (side) they are on
+    */
     socket.on('playerSwitchBase', function(data) {
         var res = mobile.playerDirection(socket, data, housekeeping.logger);
 
@@ -155,7 +180,10 @@ io.on('connection', function(socket) {
     /* 
         --------------------------
         Unity events
-        ------------------------
+        --------------------------
+    */
+    /*
+        A player has respawned in game    
     */
     socket.on('gamePlayerRespawn', function(data) {
         var res = unity.gamePlayerRespawn(socket, data, housekeeping.logger, playerList);
@@ -165,6 +193,9 @@ io.on('connection', function(socket) {
         }
     });
 
+    /*
+        A player has died in game
+    */
     socket.on('gamePlayerDied', function(data) {
         var res = unity.gamePlayerDied(socket, data, housekeeping.logger);
 
@@ -173,6 +204,9 @@ io.on('connection', function(socket) {
         }
     });
 
+    /*
+        The game state has been updated
+    */
     socket.on('gameStateUpdate', function(data) {
         var res = unity.gameStateUpdate(socket, data, housekeeping.logger);
 
@@ -182,7 +216,7 @@ io.on('connection', function(socket) {
     });
 
     /*
-        Called when a player's health is changed
+        A player's health has changed
     */
     socket.on('gamePlayerChangeHealth', function(data) {
         var res = unity.gamePlayerChangeHealth(socket, data, housekeeping.logger, playerList);
@@ -193,7 +227,7 @@ io.on('connection', function(socket) {
     });
 
     /*
-        Called by Unity when a player is near their base
+        A player is now near their own base
 
         Informs the player that are near the base
         Allows the player to do things like upgrade or switch lanes
@@ -207,7 +241,7 @@ io.on('connection', function(socket) {
     });
 
     /*
-        Called by Unity when a player has successfuly joined the game
+        A player has successfully joined the game
 
         Updates that players info to what team they have been asssigned to
         Also broadcasts this event to all clients in that game to update their own player list
