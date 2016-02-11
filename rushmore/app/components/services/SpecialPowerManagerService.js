1335ff6c0974b5314bd31ccdf774b245972019c4
James Hughes <jh13293@my.bristol.ac.uk>
2016-02-11 13:09:35 +0000

    remove alert

--- rushmore/app/components/services/SpecialPowerManagerService.js
+++ rushmore/app/components/services/SpecialPowerManagerService.js
@@ -5,31 +5,25 @@
 angular.module('myApp').factory('SpecialPowerManagerService', function($q, $interval) {
 
     var cooldownTIme = 5000;
+    var vibrateTime = 200;
 
     var specialButtonUsed = function(special) {
         var deferred = $q.defer();
 
-        navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
-
-        // if(navigator.vibrate) {
-        //     alert("vibration supported");
-        // }
-
         // vibrate the phone for 200 miliseconds
-        var success = window.navigator.vibrate(1000);
-        alert(success);
+        window.navigator.vibrate(vibrateTime);
 
         special.enabled = "special-disabled";
 
         var specialCooldownTimer = $interval(function() {
             // capture special used in closue to allow multple timeouts
-            var spec = special;         
+            var spec = special;
 
             // Cancle timer and reset special
             $interval.cancel(specialCooldownTimer);
             spec.enabled = "";
             deferred.resolve(spec);
-            
+
         }, cooldownTIme);
 
         return deferred.promise;
