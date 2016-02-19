5014a34a2dba4dcaf7c33c6d88d8d4c98041bfc3
James Hughes <jh13293@my.bristol.ac.uk>
2016-02-19 12:17:32 +0000

    add local socket and pure files for local modes

--- rushmore/app/components/config.js
+++ rushmore/app/components/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('ENV', {name:'development',socketIOEndpoint:'http://localhost:1337'})
+.constant('ENV', {name:'development',socketIOEndpoint:'http://192.168.1.3:1337'})
 
 ;
