e8066fd63098e3260db0332c3f2c1f381e31477f
James Hughes <jh13293@my.bristol.ac.uk>
2016-02-21 16:22:13 +0000

    add mutliple tutorial screens

--- rushmore/app/views/tutorials/tutorial.js
+++ rushmore/app/views/tutorials/tutorial.js
@@ -3,29 +3,92 @@
    Let the player move throough a tutorial section of pages
 */
 angular.module('tutorialView', ['ngRoute'])
-.controller('TutorialCtrl', ['$scope', 'UserService', 'LocationService', function($scope, UserService, LocationService) {
+    .controller('TutorialCtrl', ['$scope', 'UserService', 'LocationService', function($scope, UserService, LocationService) {
 
 
-    $scope.tutorialSteps = 3;
-    $scope.currentStep = 0;
-    
-   
-    $scope.next = function() {
-        console.log("NEXT");
+        $scope.tutorialSteps = 3;
+        $scope.currentTutorialIndex = 0;
+        $scope.nextText = "NEXT";
 
-        $scope.currentStep += 1;
+        $scope.tutorials = [{
+            numLessons: 3,
+            tutIndex: 0,
+            lessons: [{
+                lessonIndex: 0,
+                lessonText: "You are a powerful hero",
+                lessonImage: "../../resources/images/eye_black.png"
+            }, {
+                lessonIndex: 1,
+                lessonText: "Destroy the enemy base to win",
+                lessonImage: "../../resources/images/eye_black.png"
+            }, {
+                lessonIndex: 2,
+                lessonText: "Kill enemy grunts to increase your power",
+                lessonImage: "../../resources/images/heart_green.png"
+            }]
+        },{
+            numLessons: 3,
+            tutIndex: 1,
+            lessons: [{
+                lessonIndex: 0,
+                lessonText: "FUCKING GAY BOY",
+                lessonImage: "../../resources/images/heart_black.png"
+            }, {
+                lessonIndex: 1,
+                lessonText: "You are a powerful hero",
+                lessonImage: "../../resources/images/eye_black.png"
+            }, {
+                lessonIndex: 2,
+                lessonText: "I'M A STINKY POOP",
+                lessonImage: "../../resources/images/eye_purple.png"
+            }]
+        },{
+            numLessons: 3,
+            tutIndex: 2,
+            lessons: [{
+                lessonIndex: 0,
+                lessonText: "ANOTHER ONE",
+                lessonImage: "../../resources/images/eye_black.png"
+            }, {
+                lessonIndex: 1,
+                lessonText: "You are a powerful hero",
+                lessonImage: "../../resources/images/flame_red.png"
+            }, {
+                lessonIndex: 2,
+                lessonText: "The key to success is the key",
+                lessonImage: "../../resources/images/eye_black.png"
+            }]
+        }];
 
-        if($scope.currentStep < $scope.tutorialSteps) {
-            // move to the next tutorial
-        } else {
-            // finished tutorial, go to the lobby
+        $scope.next = function() {
+            if ($scope.currentTutorialIndex + 1 < $scope.tutorialSteps) {
+                // move to the next tutorial
+                $scope.currentTutorialIndex += 1;
+                $scope.nextText = "NEXT";
+
+                // Indicate they can move to the lobby on the last tutorial page
+                if($scope.currentTutorialIndex + 1 === $scope.tutorialSteps) {
+                    $scope.nextText = "LOBBY";
+                }
+
+            } else {
+                // finished tutorial, go to the lobby
+                LocationService.setPath('/lobby');
+            }
+        };
+
+        $scope.prev = function() {
+            if ($scope.currentTutorialIndex > 0) {
+                // move to the next tutorial
+                $scope.currentTutorialIndex -= 1;
+                $scope.nextText = "NEXT";
+            } else {
+                // Can't go back any further 
+            }
+        };
+
+        $scope.skip = function() {
             LocationService.setPath('/lobby');
-        }
-        
-    };
+        };
 
-    $scope.skip = function() {
-        LocationService.setPath('/lobby');
-    };
-
-}]);
+    }]);
