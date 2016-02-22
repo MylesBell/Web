4300fa4253ec56e9c8cf1b605ba4e4d76cefd8f3
James Hughes <jh13293@my.bristol.ac.uk>
2016-02-22 19:09:21 +0000

    add sliding tutorial pages for next

--- rushmore/app/views/tutorials/tutorial.js
+++ rushmore/app/views/tutorials/tutorial.js
@@ -11,39 +11,71 @@ angular.module('tutorialView', ['ngRoute'])
         $scope.nextText = "NEXT";
         $scope.prevText = "SKIP";
 
+        setTeamBackground();
+
         // TODO pull this out to a service
         $scope.tutorials = [{
             tutIndex: 0,
             tutorialTitle: "You are a powerful hero",
             tutorialText: "Defeating enemy grunts and heros will make you stronger",
-            tutorialImage: "../../resources/images/eye_black.png",
+            tutorialImage: "../../resources/images/base_cowboy.png",
+            visible: true
         }, {
             tutIndex: 1,
             tutorialTitle: "Destroy the enemy's base to win",
             tutorialText: "Your team's grunts will help to keep the enemy from your gates",
-            tutorialImage: "../../resources/images/eye_black.png",
-
+            tutorialImage: "../../resources/images/base_cowboy.png",
+            visible: false
         }, {
             tutIndex: 2,
             tutorialText: "Deating ",
-            tutorialImage: "../../resources/images/eye_black.png",
+            tutorialImage: "../../resources/images/base_cowboy.png",
+            visible: false
         }];
 
+        $scope.getStyle = function(tut) {
+            var style = {
+                background_image: "linear-gradient(to bottom, rgba(255,255,255,0) 70%,rgba(0,0,0,0.6) 100%), url('" + tut.tutorialImage + "')"
+            };
+            return style;
+        };
+
         $scope.next = function() {
+
             if ($scope.currentTutorialIndex + 1 < $scope.tutorialSteps) {
-                // move to the next tutorial
-                $scope.currentTutorialIndex += 1;
+
                 $scope.nextText = "NEXT";
                 $scope.prevText = "PREV";
 
-                // Indicate they can move to the lobby on the last tutorial page
-                if ($scope.currentTutorialIndex + 1 === $scope.tutorialSteps) {
-                    $scope.nextText = "LOBBY";
-                }
+                // animate the current page moving out
+                var prevTutPage = document.getElementById("tutorial-" + $scope.currentTutorialIndex);
+                prevTutPage.classList.add("tutorial-anim-page-slide-left");
 
-                if ($scope.currentTutorialIndex === 0) {
-                    $scope.prevText = "SKIP";
-                }
+                // when the animation ends, hide the old tutorial page
+                prevTutPage.addEventListener("transitionend", function() {
+                    $scope.tutorials[$scope.currentTutorialIndex].visible = false;
+
+                    // move to the next tutorial
+                    $scope.currentTutorialIndex += 1;
+                    $scope.$apply();
+
+                    //Update the navgation text
+                    if ($scope.currentTutorialIndex === $scope.tutorialSteps) {
+                        // Indicate they can move to the lobby on the last tutorial page
+                        $scope.nextText = "TO LOBBY";
+                    }
+
+                    if ($scope.currentTutorialIndex === 0) {
+                        // allow skipping on the first page
+                        $scope.prevText = "SKIP";
+                    }
+                });
+
+                // animate the next lesson moving in
+                var nextTutPage = document.getElementById("tutorial-" + ($scope.currentTutorialIndex + 1));
+                // set the next lesson to visible
+                $scope.tutorials[$scope.currentTutorialIndex + 1].visible = true;
+                nextTutPage.classList.add("tutorial-anim-page-slide-in-left");
 
             } else {
                 // finished tutorial, go to the lobby
@@ -52,6 +84,7 @@ angular.module('tutorialView', ['ngRoute'])
         };
 
         $scope.prev = function() {
+            console.log("prev");
             if ($scope.currentTutorialIndex > 0) {
                 // move to the next tutorial
                 $scope.currentTutorialIndex -= 1;
@@ -72,5 +105,15 @@ angular.module('tutorialView', ['ngRoute'])
         $scope.skip = function() {
             LocationService.setPath('/lobby');
         };
+
+        // Change the background colour of the container to the teams colours
+        function setTeamBackground() {
+            var colors = UserService.getTeamColor();
+
+            var tutorialsControlContainer = document.getElementById('tutorial-controls');
+
+            tutorialsControlContainer.style.backgroundColor = colors.dark;
+
+        }
 
     }]);
