4b86ebf1cc3c2d85ecd46720b1f53f9516c66333
James Hughes <jh13293@my.bristol.ac.uk>
2016-03-18 09:48:49 +0000

    don't allow user to not enter a name

--- rushmore/app/views/playerCreation/playerCreation.js
+++ rushmore/app/views/playerCreation/playerCreation.js
@@ -3,33 +3,36 @@
    Let the player set their username, then move to the game join screen
 */
 angular.module('playerCreationView', ['ngRoute'])
-.controller('PlayerCreationCtrl', ['$scope', 'UserService', 'LocationService', function($scope, UserService, LocationService) {
+    .controller('PlayerCreationCtrl', ['$scope', 'UserService', 'LocationService', function($scope, UserService, LocationService) {
 
+        // Entered name of the user
+        $scope.username = "Enter your name";
 
-    // Entered name of the user
-    $scope.username = "Enter your name";
-    
-    /* 
-        Called when the deploy button selected
-            register username to the server over the socket
-            move to the main game screen
-    */
-    $scope.deploy = function() {
+        /* 
+            Called when the deploy button selected
+                register username to the server over the socket
+                move to the main game screen
+        */
+        $scope.deploy = function() {
 
-        //register with server and send username
-        // TODO save user details, perhaps in the user service
+            //register with server and send username
+            // TODO save user details, perhaps in the user service
+            if ($scope, username === "Enter your name") {
+                console.log("didn't enter a name");
+            } else {
+                UserService.registerUserWithServer($scope.username)
+                    .then(function(res) {
+                        console.log(res);
+                        LocationService.setPath('/join'); // TODO CHANGE THIS 
 
-        UserService.registerUserWithServer($scope.username)
-            .then(function(res) {
-                console.log(res);
-                LocationService.setPath('/join'); // TODO CHANGE THIS 
+                    }).catch(function(res) {
 
-             }).catch(function(res) {
-                
-                // name was not right, show the user the error                
-                $scope.username = res.message; 
-                console.log(res);
-            });
-    };
+                        // name was not right, show the user the error                
+                        $scope.username = res.message;
+                        console.log(res);
+                    });
+            }
 
-}]);
+        };
+
+    }]);
