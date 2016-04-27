ad6f04736bdf496e5d9c209085c8d3912c252e30
josephfoulds <josephfoulds@googlemail.com>
2016-04-27 13:46:25 +0100

    [Minification] Modify config.js variable namings for minification

--- rushmore/app/components/config.js
+++ rushmore/app/components/config.js
@@ -1,5 +1,1 @@
-angular.module('config', [])
-
-.constant('ENV', {name:'development',socketIOEndpoint:'http://localhost:1337'})
-
-;
+angular.module('config', []).constant('ENV', {name:'development',socketIOEndpoint:'http://localhost:1337'});
