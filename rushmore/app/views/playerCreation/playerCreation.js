222b4ccbe2c1c87d805314b4b33fd9621e87780b
James Hughes <jh13293@my.bristol.ac.uk>
2016-05-01 19:17:31 +0100

    update css for playerjoin, don't allow button press when name not correct

--- rushmore/app/views/playerCreation/playerCreation.js
+++ rushmore/app/views/playerCreation/playerCreation.js
@@ -3,29 +3,42 @@
    Let the player set their username, then move to the game join screen
 */
 angular.module('playerCreationView', ['ngRoute'])
-    .controller('PlayerCreationCtrl', ['$scope', 'UserService', 'LocationService', function($scope, UserService, LocationService) {
+    .controller('PlayerCreationCtrl', ['$scope', 'UserService', 'LocationService', function ($scope, UserService, LocationService) {
 
         // Entered name of the user
         $scope.username = "";
+        // Css class showing wether the input name is valid or not
+        $scope.validname = false;
+
+        $scope.checkName = function () {
+            if ($scope.username === "") {
+                console.log("didn't enter a name");
+                $scope.validname = false;
+            } else if ($scope.username.length > 20) {
+                console.log("Too long");
+                $scope.validname = false;
+            } else {
+                $scope.validname = true;
+            }
+        }
 
         /* 
             Called when the deploy button selected
                 register username to the server over the socket
                 move to the main game screen
         */
-        $scope.deploy = function() {
+        $scope.deploy = function () {
 
             //register with server and send username
             // TODO save user details, perhaps in the user service
-            if ($scope.username === "Enter your name") {
-                console.log("didn't enter a name");
-            } else {
+            if ($scope.validname) {
+
                 UserService.registerUserWithServer($scope.username)
-                    .then(function(res) {
+                    .then(function (res) {
                         console.log(res);
                         LocationService.setPath('/join'); // TODO CHANGE THIS 
 
-                    }).catch(function(res) {
+                    }).catch(function (res) {
 
                         // name was not right, show the user the error                
                         $scope.username = res.message;
