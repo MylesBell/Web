b607f9b254de3cd85ef41ad0ad70ad4ae3da0f31
josephfoulds <josephfoulds@googlemail.com>
2016-05-04 14:29:48 +0100

    Fix config

--- rushmore/app/components/config.js
+++ rushmore/app/components/config.js
@@ -1,1 +1,5 @@
-angular.module('config', []).constant('ENV', {name:'development',socketIOEndpoint:'http://localhost:1337'});
+angular.module('config', [])
+
+.constant('ENV', {name:'development',socketIOEndpoint:'http://localhost:1337',skipCode:false})
+
+;
