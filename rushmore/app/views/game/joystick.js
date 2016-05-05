0564ba8254905618a2879ef39228eaa0d74f3503
James Hughes <jh13293@my.bristol.ac.uk>
2016-05-05 15:19:32 +0100

    disbable background paralax on game controller

--- rushmore/app/views/game/joystick.js
+++ rushmore/app/views/game/joystick.js
@@ -304,8 +304,8 @@ angular.module('gameView')
                 var backgroundPos = [['30%', '0%'], ['10%', '10%'], ['0%', '30%'], ['-10%', '10%'], ['-30%', '0%'], ['-10%', '-10%'],['0%', '-30%'], ['10%', '-10%']];
                 console.log(movDir);
                 // move the background around
-                background.style.backgroundPositionX = backgroundPos[movDir][0];
-                background.style.backgroundPositionY = backgroundPos[movDir][1];
+                // background.style.backgroundPositionX = backgroundPos[movDir][0];
+                // background.style.backgroundPositionY = backgroundPos[movDir][1];
 
             } else if (movDir !== newMoveDir && newMoveDir === -1) {
                 // dead zone
