72255d3854a81d2e6bc45ca119fb32ef421c4729
James Hughes <jh13293@my.bristol.ac.uk>
2016-05-10 09:19:29 +0100

    fix socket endpoint broken

--- rushmore/app/components/config.js
+++ rushmore/app/components/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('ENV', {name:'development',socketIOEndpoint:'http://localhost:1337',skipCode:false})
+.constant('ENV', {name:'production',socketIOEndpoint:'http://headgearsofwar.com:1337',skipCode:true})
 
 ;
